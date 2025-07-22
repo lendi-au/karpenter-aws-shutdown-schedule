acab24fde185a95b93ba535c7542312b810f4d20
Ed Kim <edify42@yahoo.com>
2025-07-22 21:35:35 +1000

    wip stuff
    Signed-off-by: Ed Kim <edify42@yahoo.com>

--- lambda/main.go
+++ lambda/main.go
@@ -15,80 +15,9 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/aws-iam-authenticator/pkg/token"
 )
-
-func newClientset(ctx context.Context) (*kubernetes.Clientset, error) {
-	clusterName := os.Getenv("KUBERNETES_CLUSTER_NAME")
-	if clusterName == "" {
-		return nil, fmt.Errorf("KUBERNETES_CLUSTER_NAME environment variable not set")
-	}
-
-	region := utils.GetenvDefault("AWS_REGION", "ap-southeast-2") // lambda knows what region it's in right?
-
-	cfg, err := config.LoadDefaultConfig(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("error creating AWS session: %w", err)
-	}
-
-	gen, err := token.NewGenerator(true, false)
-	if err != nil {
-		return nil, fmt.Errorf("error creating token generator: %w", err)
-	}
-
-	stsClient := sts.NewFromConfig(cfg)
-	result, err := stsClient.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
-	if err != nil {
-		return nil, err
-	}
-	accountId := *result.Account
-	fmt.Print(accountId)
-	// roleArn := fmt.Sprintf("arn:aws:iam::%s:role/karpenter-ec2-instance-stop-start", accountId)
-
-	opts := &token.GetTokenOptions{
-		// AssumeRoleARN: "arn:aws:iam::<account_id>:role/<role-name>", // Consider supporting this via config...
-		ClusterID: clusterName,
-		Region:    region,
-	}
-	tok, err := gen.GetWithOptions(opts)
-
-	if err != nil {
-		return nil, fmt.Errorf("error generating token: %w", err)
-	}
-
-	eksClient := eks.NewFromConfig(cfg)
-	out, err := eksClient.DescribeCluster(ctx, &eks.DescribeClusterInput{
-		Name: &clusterName,
-	})
-	if err != nil {
-		return nil, err
-	}
-	caBase64 := *out.Cluster.CertificateAuthority.Data
-	ca, err := base64.StdEncoding.DecodeString(caBase64)
-	if err != nil {
-		return nil, err
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	config := &rest.Config{
-		Host:        os.Getenv("KUBERNETES_SERVICE_HOST"),
-		BearerToken: tok.Token,
-		TLSClientConfig: rest.TLSClientConfig{
-			CAData: ca,
-		},
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, fmt.Errorf("error creating Kubernetes clientset: %w", err)
-	}
-
-	return clientset, nil
-}
 
 func newDynamicClient(ctx context.Context) (*dynamic.DynamicClient, error) {
 	clusterName := os.Getenv("KUBERNETES_CLUSTER_NAME")
@@ -161,22 +90,19 @@ type ActionEvent struct {
 
 func handler(ctx context.Context, request ActionEvent) error {
 	fmt.Printf("ctx: %v", ctx)
-	fmt.Printf("request: %v", request)
+	fmt.Printf("Requested action: %s", request.Action)
 
 	nodePoolName := os.Getenv("KARPENTER_NODEPOOL_NAME")
 	if nodePoolName == "" {
 		return fmt.Errorf("KARPENTER_NODEPOOL_NAME environment variable not set")
 	}
 
-	// This is a placeholder for the actual Karpenter API interaction
-	// You would need to use the Karpenter client to update the NodePool
-	fmt.Printf("Simulating scaling down nodepool %s\n", nodePoolName)
-
 	dynamicClient, err := newDynamicClient(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to create dynamic client: %v", err)
 	}
 
+	// common const setup
 	nodePoolGVR := schema.GroupVersionResource{
 		Group:    "karpenter.sh",
 		Version:  "v1",
@@ -188,17 +114,37 @@ func handler(ctx context.Context, request ActionEvent) error {
 		return fmt.Errorf("failed to get nodepool %s: %v", nodePoolName, err)
 	}
 
-	err = unstructured.SetNestedField(np.Object, "0", "spec", "limits", "cpu")
-	if err != nil {
-		return fmt.Errorf("failed to set cpu limit for nodepool %s: %v", nodePoolName, err)
-	}
+	switch request.Action {
+	case "shutdown":
+		fmt.Printf("Simulating scaling down nodepool %s\n", nodePoolName)
+		err = unstructured.SetNestedField(np.Object, "0", "spec", "limits", "cpu")
+		if err != nil {
+			return fmt.Errorf("failed to set cpu limit for nodepool %s: %v", nodePoolName, err)
+		}
 
-	_, err = dynamicClient.Resource(nodePoolGVR).Update(ctx, np, metav1.UpdateOptions{})
-	if err != nil {
-		return fmt.Errorf("failed to update nodepool %s: %v", nodePoolName, err)
-	}
+		_, err = dynamicClient.Resource(nodePoolGVR).Update(ctx, np, metav1.UpdateOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to update nodepool %s: %v", nodePoolName, err)
+		}
 
-	fmt.Printf("Successfully updated nodepool %s to set cpu limit to 0\n", nodePoolName)
+		fmt.Printf("Successfully updated nodepool %s to set cpu limit to 0\n", nodePoolName)
+	case "startup":
+		fmt.Printf("Simulating scale up of nodepool %s\n", nodePoolName)
+		cpuLimit := os.Getenv("KARPENTER_NODEPOOL_LIMITS_CPU")
+		if cpuLimit == "" {
+			fmt.Printf("Environment variable KARPENTER_NODEPOOL_LIMITS_CPU not set - using default 1000")
+			cpuLimit = "1000"
+		}
+		err = unstructured.SetNestedField(np.Object, cpuLimit, "spec", "limits", "cpu")
+		if err != nil {
+			return fmt.Errorf("failed to set cpu limit for nodepool %s: %v", nodePoolName, err)
+		}
+
+		_, err = dynamicClient.Resource(nodePoolGVR).Update(ctx, np, metav1.UpdateOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to update nodepool %s: %v", nodePoolName, err)
+		}
+	}
 
 	// EC2 interaction
 	if err := ShutdownEC2Instances(ctx); err != nil {
